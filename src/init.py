573bb931d149c1ec904af3b4cfbffb91a7283238
Amit <lordamit@gmail.com>
2013-09-17 15:46:30 +0600

    license details added
    Following the instructions of GPL
     -> each source file now contains copy right notice
     -> separate license file
     -> separate GPL license file

--- src/init.py
+++ src/init.py
@@ -2,6 +2,21 @@
 
 # -*- coding: utf-8 -*-
 
+#This file is part of Brightness Controller.
+#
+#Brightness Controller is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Brightness Controller is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 from PySide import QtGui, QtCore
 from ui.mainwindow import Ui_MainWindow
 from ui.license import Ui_Form as License_Ui_Form
